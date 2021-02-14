2eff9618089527c1711a914dcf24306a9236c492
Christos <987149+christosle@users.noreply.github.com>
2021-02-14 19:05:29 +0200

    Read package.json in a separate file than usePackageInfo

---
+++ src/helpers/packageInfo.ts
@@ -0,0 +1,10 @@
+import { name, productName, description, version } from '../../package.json'
+
+export const packageInfo = {
+  name,
+  productName,
+  description,
+  version
+}
+
+export default packageInfo
--- src/use/usePackageInfo.ts
+++ src/use/usePackageInfo.ts
@@ -2,20 +2,16 @@ import {
   computed
 } from 'vue'
 import { useStore } from 'vuex'
-import { name, productName, description, version } from '../../package.json'
+import { packageInfo } from 'src/helpers/packageInfo'
 
 export default function usePackageInfo () {
   const store = useStore()
 
   function setPackageInfo () {
-    const name1: string = name
-    const productName1: string = productName
-    const description1: string = description
-    const version1: string = version
-    void store.dispatch('packageInfo/setName', name1, { root: true })
-    void store.dispatch('packageInfo/setProductName', productName1, { root: true })
-    void store.dispatch('packageInfo/setDescription', description1, { root: true })
-    void store.dispatch('packageInfo/setVersion', version1, { root: true })
+    void store.dispatch('packageInfo/setName', packageInfo.name, { root: true })
+    void store.dispatch('packageInfo/setProductName', packageInfo.productName, { root: true })
+    void store.dispatch('packageInfo/setDescription', packageInfo.description, { root: true })
+    void store.dispatch('packageInfo/setVersion', packageInfo.version, { root: true })
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
